e61efb4f21d6e74056eceaefb0fadf41bafb0520
andrey20005 <z.andrey2005.10@gmail.com>
2026-02-24 18:41:12 +0300

    нормальная форма

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -20,29 +20,34 @@ const rValues = [-4, -3, -2, -1, 0, 1, 2, 3, 4]
             <ImageInForm />
         </ItemBox>
         <ItemBox id="form">
-            <InputField>
-                <template #legend>X:</template>
-                <select v-model.number="pointsStore.newPoint.x">
-                    <option v-for="val in xValues" :key="val" :value="val">
-                        {{ val }}
-                    </option>
-                </select>
-            </InputField>
-            <InputField>
-                <template #legend>Y:</template>
-                <input type="number" v-model.number="pointsStore.newPoint.y" step="0.5" min="-3" max="3"
-                    placeholder="от -3 до 3" />
-            </InputField>
-            <InputField>
-                <template #legend>R:</template>
-                <select v-model.number="pointsStore.newPoint.r">
-                    <option v-for="val in rValues" :key="val" :value="val">
-                        {{ val }}
-                    </option>
-                </select>
-            </InputField>
-            <a @click="pointsStore.addPoint()">{{ pointsStore.loading ? 'Добавление...' : 'Добавить' }}</a>
-            <div class="exeptoin_in_form" v-if="pointsStore.error">{{ pointsStore.error }}</div>
+            <form @submit.prevent="pointsStore.addPoint()">
+                <InputField>
+                    <template #legend>X:</template>
+                    <select v-model.number="pointsStore.newPoint.x">
+                        <option v-for="val in xValues" :key="val" :value="val">
+                            {{ val }}
+                        </option>
+                    </select>
+                </InputField>
+                <InputField>
+                    <template #legend>Y:</template>
+                    <input type="number" v-model.number="pointsStore.newPoint.y" min="-3" step="0.0001" max="3"
+                        placeholder="от -3 до 3" />
+                </InputField>
+                <InputField>
+                    <template #legend>R:</template>
+                    <select v-model.number="pointsStore.newPoint.r">
+                        <option v-for="val in rValues" :key="val" :value="val">
+                            {{ val }}
+                        </option>
+                    </select>
+                </InputField>
+                <InputField>
+                    <button type="submit" :disabled="pointsStore.loading"> {{ pointsStore.loading ? 'Добавление...' : 'Добавить' }} </button>
+                </InputField>
+                <div class="exeptoin_in_form" v-if="pointsStore.error">{{ pointsStore.error }}</div>
+            </form>
+            <!-- <a @click="pointsStore.addPoint()">{{ pointsStore.loading ? 'Добавление...' : 'Добавить' }}</a> -->
         </ItemBox>
         <ItemBox style="flex: 4">
             <ResTable />
